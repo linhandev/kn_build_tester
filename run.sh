ff832fa6069241aa1eb169d95c65964fa231139f
linhandev <linhandev@qq.com>
2025-09-16 10:19:35 +0800

    nit: chang to cpp

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ set -ex
 ./gradlew linkDebugSharedOhosArm64
 
 cd c-caller
-/Users/user/.konan/dependencies/llvm-19.1.7-aarch64-macos-ohos-2/bin/clang \
+/Users/user/.konan/dependencies/llvm-19.1.7-aarch64-macos-ohos-2/bin/clang++ \
       --sysroot /Users/user/.konan/dependencies/sysroot-ohos-aarch64-5.0.11.110 \
       --target=aarch64-linux-ohos \
       -fPIC -pthread \
-      -Wall -Wextra -std=c99 \
+      -Wall -Wextra -std=c++17 \
       -I../build/bin/ohosArm64/debugShared \
-      -o main main.c \
+      -o main main.cpp \
       -L../build/bin/ohosArm64/debugShared \
       -lc2k
 
