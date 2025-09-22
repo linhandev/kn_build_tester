cf629cbd65537d2e0d0430af3cfc79e1a6c80585
linhandev <linhandev@qq.com>
2025-09-22 14:47:05 +0800

    nit: run in simulator

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ set -ex
 
 ./gradlew linkDebugExecutableNative --rerun-tasks
 
-hdc file send build/bin/native/debugExecutable/kn-sample.kexe /data/
-hdc shell chmod 777 /data/kn-sample.kexe
-hdc shell LD_PRELOAD=/data/app/el1/bundle/public/com.huawei.hmos.location/libs/arm64/libc++_shared.so /data/kn-sample.kexe
+hdc file send build/bin/native/debugExecutable/kn-sample.kexe /data/local/tmp
+hdc shell chmod a+x /data/local/tmp/kn-sample.kexe
+hdc shell LD_PRELOAD=/data/app/el1/bundle/public/com.huawei.hmos.location/libs/arm64/libc++_shared.so /data/local/tmp/kn-sample.kexe
