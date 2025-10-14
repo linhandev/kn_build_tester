298bde8dcb72a77a9b6538136324e9569ef92333
linhandev <linhandev@qq.com>
2025-10-14 21:52:33 +0800

    nit: rm docstring

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,11 +1,5 @@
 @file:OptIn(kotlin.experimental.ExperimentalNativeApi::class)
 
-/**
- * Simple subtraction function that can be called from C
- * @param a First number
- * @param b Second number  
- * @return Difference of a - b
- */
 @CName("subtract_numbers")
 fun subtractNumbers(a: Int, b: Int): Int {
     return a - b
