4891cd3a24c2b36d21001c10aa514a83b1e8affa
linhandev <linhandev@qq.com>
2025-12-23 11:02:53 +0800

    feat: add demo plugin

--- MultiModule/hvigorfile.ts
+++ MultiModule/hvigorfile.ts
@@ -1,6 +1,17 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import { HvigorPlugin, HvigorNode } from '@ohos/hvigor';
+
+function customPlugin(): HvigorPlugin {
+  return {
+    pluginId: 'customPlugin',
+    apply(node: HvigorNode) {
+      // 插件主体
+      console.log('hello customPlugin!');
+    }
+  }
+}
 
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [customPlugin()]       /* Custom plugin to extend the functionality of Hvigor. */
 }
